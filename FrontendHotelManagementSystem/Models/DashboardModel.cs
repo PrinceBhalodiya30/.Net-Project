0557bdc4e672950413cfd991b784b650b99ca5c5
PrinceBhalodiya30 <146628141+PrinceBhalodiya30@users.noreply.github.com>
2025-07-28 18:31:05 +0530

    Delete FrontendHotelManagementSystem/Models directory

--- FrontendHotelManagementSystem/Models/DashboardModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace FrontendHotelManagementSystem.Models
-{
-    public class DashboardModel
-    {
-        
-    }
-}
--- FrontendHotelManagementSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FrontendHotelManagementSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
